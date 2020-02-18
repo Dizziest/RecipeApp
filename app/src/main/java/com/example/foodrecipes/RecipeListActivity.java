1775506483082ad9282e9e9a3c16b41df116bd29
Dizziest <marcin.michalowski45@gmail.com>
2020-02-18 14:48:17 +0100

    Added Retrofit logic, models for Recipe and Responses

--- app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -1,10 +1,27 @@
 package com.example.foodrecipes;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.example.foodrecipes.models.Recipe;
+import com.example.foodrecipes.requests.RecipeApi;
+import com.example.foodrecipes.requests.ServiceGenerator;
+import com.example.foodrecipes.requests.responses.RecipeResponse;
+import com.example.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
 
+    private static final String TAG = "RecipeListActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,11 +30,38 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mProgressBar.getVisibility() == View.VISIBLE){
-                    showProgressBar(false);
+                testRetrofitRequest();
+            }
+        });
+    }
+
+    private void testRetrofitRequest(){
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+
+        Call<RecipeResponse> responseCall = recipeApi
+                .getRecipe(
+                        "35172"
+                        );
+
+        responseCall.enqueue(new Callback<RecipeResponse>() {
+            @Override
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                if (response.code() == 200) {
+                    Log.d(TAG, "onResponse: " + response.body().toString());
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: " + recipe.toString());
                 } else {
-                    showProgressBar(true);
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
+
             }
         });
     }
---
+++ app/src/main/java/com/example/foodrecipes/util/Constants.java
@@ -0,0 +1,10 @@
+package com.example.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+
+    public static final String API_KEY = "";
+
+
+}
