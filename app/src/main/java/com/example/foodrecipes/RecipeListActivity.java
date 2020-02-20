60301d291150096d08b9a77fe88f6f8cf6d1133e
Dizziest <marcin.michalowski45@gmail.com>
2020-02-20 14:43:58 +0100

    Added RecyclerView and RecipeAdapter for recipes. Added Toolbar with SearchView and logic to search

--- app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -1,12 +1,15 @@
 package com.example.foodrecipes;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodrecipes.adapters.OnRecipeListener;
+import com.example.foodrecipes.adapters.RecipeRecyclerAdapter;
 import com.example.foodrecipes.models.Recipe;
 
 import com.example.foodrecipes.util.Testing;
@@ -17,26 +20,25 @@ import java.util.List;
 
 
 
-public class RecipeListActivity extends BaseActivity {
+public class RecipeListActivity extends BaseActivity implements OnRecipeListener {
 
     private static final String TAG = "RecipeListActivity";
 
     private RecipeListViewModel mRecipeListViewModel;
+    private RecyclerView mRecyclerView;
+    private RecipeRecyclerAdapter mAdapter;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        mRecyclerView = findViewById(R.id.recipe_list);
 
         mRecipeListViewModel = new ViewModelProvider(this).get(RecipeListViewModel.class);
 
+        initRecyclerView();
         subscribeObservers();
-        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                testRetrofitRequest();
-            }
-        });
+        initSearchView();
     }
 
     private void subscribeObservers(){
@@ -46,15 +48,42 @@ public class RecipeListActivity extends BaseActivity {
                 if (recipes != null) {
                     Testing.printRecipes(recipes, "recipes test");
                 }
+                mAdapter.setRecipes(recipes);
             }
         });
     }
 
-    private void searchRecipesApi(String query, int pageNumber){
-        mRecipeListViewModel.searchRecipesApi(query, pageNumber);
+    private void initRecyclerView(){
+        mAdapter = new RecipeRecyclerAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void testRetrofitRequest(){
-        searchRecipesApi("chicken", 1);
+
+    private void initSearchView(){
+        final androidx.appcompat.widget.SearchView searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mRecipeListViewModel.searchRecipesApi(query, 1);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    public void onRecipeClick(int position) {
+
+    }
+
+    @Override
+    public void onCategoryClick(String category) {
+
     }
 }
--- app/src/main/java/com/example/foodrecipes/util/Constants.java
+++ app/src/main/java/com/example/foodrecipes/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String BASE_URL = "https://recipesapi.herokuapp.com";
 
-    public static final int NETWORK_TIMEOUT = 3000;
+    public static final int NETWORK_TIMEOUT = 5000;
 
 
 }
